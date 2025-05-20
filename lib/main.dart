1d0994181dbe109af2c91416c3906214e3f80772
Lipták Péter <liptak.peti.98@gmail.com>
2025-05-20 12:42:54 +0200

    refactor: remove test-specific app structure and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -46,21 +45,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Make sure all required dependencies are registered for tests
-    _ensureDependenciesRegistered();
-
-    // Check if we're running in a test environment
-    final bool isTest = WidgetsBinding.instance is TestWidgetsFlutterBinding;
-
-    if (isTest) {
-      // Return a test-specific app with a counter for the widget test
-      return MaterialApp(
-        home: CounterPage(),
-        debugShowCheckedModeBanner: false,
-      );
-    }
-
-    // Return the real app for normal usage
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => LoginModel()),
@@ -69,61 +53,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color(0xFF6750A4),
-            brightness: Brightness.light,
-            secondary: const Color(0xFF03DAC6),
-            tertiary: const Color(0xFFEF5350),
-          ),
-          appBarTheme: const AppBarTheme(
-            centerTitle: true,
-            elevation: 0,
-            backgroundColor: Color(0xFF6750A4),
-            foregroundColor: Colors.white,
-            titleTextStyle: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1.2,
-            ),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-              elevation: 3,
-            ),
-          ),
-          inputDecorationTheme: InputDecorationTheme(
-            filled: true,
-            fillColor: Colors.grey.shade100,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: BorderSide.none,
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: const BorderSide(color: Color(0xFF6750A4), width: 2),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: BorderSide(color: Colors.grey.shade300),
-            ),
-            errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: BorderSide(color: Colors.red.shade300),
-            ),
-            contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-          ),
-          cardTheme: CardTheme(
-            elevation: 4,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-            clipBehavior: Clip.antiAlias,
-          ),
-          snackBarTheme: SnackBarThemeData(
-            behavior: SnackBarBehavior.floating,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-          ),
+          primarySwatch: Colors.blue,
         ),
         initialRoute: '/',
         routes: {
@@ -134,89 +64,6 @@ class MyApp extends StatelessWidget {
         navigatorObservers: GetIt.I<List<NavigatorObserver>>(),
         //DO NOT MODIFY
         debugShowCheckedModeBanner: false,
-      ),
-    );
-  }
-
-  void _ensureDependenciesRegistered() {
-    try {
-      // Try to access dependencies, if they don't exist, register minimal implementations
-      GetIt.I<List<NavigatorObserver>>();
-    } catch (_) {
-      if (!GetIt.I.isRegistered<List<NavigatorObserver>>()) {
-        GetIt.I.registerSingleton<List<NavigatorObserver>>([]);
-      }
-    }
-
-    try {
-      GetIt.I<SharedPreferences>();
-    } catch (_) {
-      if (!GetIt.I.isRegistered<SharedPreferences>()) {
-        // Just skip auto-login in tests by providing a minimal implementation
-        GetIt.I.registerSingleton<SharedPreferences>(SharedPreferencesStub());
-      }
-    }
-
-    try {
-      GetIt.I<Dio>();
-    } catch (_) {
-      if (!GetIt.I.isRegistered<Dio>()) {
-        // Register a simple Dio instance for tests
-        final dio = Dio();
-        GetIt.I.registerSingleton<Dio>(dio);
-      }
-    }
-  }
-}
-
-// Simple stub implementation for tests
-class SharedPreferencesStub implements SharedPreferences {
-  @override
-  dynamic noSuchMethod(Invocation invocation) {
-    if (invocation.memberName == #getString) return null;
-    if (invocation.memberName == #remove) return Future.value(true);
-    if (invocation.memberName == #containsKey) return false;
-    return null;
-  }
-}
-
-// Simple counter page for tests
-class CounterPage extends StatefulWidget {
-  @override
-  _CounterPageState createState() => _CounterPageState();
-}
-
-class _CounterPageState extends State<CounterPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Counter'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
